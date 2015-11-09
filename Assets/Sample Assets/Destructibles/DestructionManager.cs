8aaede993d3fd8eeb20ce2852af29015fff91067
Rémi Latapy <remi.latapy@insa-lyon.fr>
2015-11-09 13:09:50 -0500

    remove lib and add new destruction system

---
+++ Assets/Sample Assets/Destructibles/DestructionManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructionManager : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		this.GetComponent<Collider>().enabled = false;
+		CarController car = other.GetComponentInParent<CarController> ();
+		if (car != null) {
+			car.modifyStyleScore (500, "BOUM ! +500");
+		}
+	}
+}
