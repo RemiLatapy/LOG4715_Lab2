239dcf22d194152f3eac0cce6b869e002450d6c5
Sonia Ghabi <sonia.ghabi@gmail.com>
2015-12-04 15:45:35 -0500

    Moving womps

---
+++ Assets/Sample Assets/Moving Obstacles/Scripts/WompScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WompScript : MonoBehaviour {
+	private float startPointX;
+	private float startPointY;
+	[SerializeField]
+	private bool horizontal;
+	private float fallSpeed;
+	bool reverse;
+
+	// Use this for initialization
+	void Start () {
+		fallSpeed = 2f;
+		startPointX = rigidbody.transform.position.x;
+		startPointY = rigidbody.transform.position.y;
+		reverse = !horizontal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (horizontal) {
+			if (!reverse) {
+				this.rigidbody.velocity = new Vector3 (5, 0, 0);
+				reverse = rigidbody.transform.position.x >= startPointX + 25f;
+			} else {
+				this.rigidbody.velocity = new Vector3 (-5, 0, 0);
+				reverse = !(rigidbody.transform.position.x <= startPointX);
+			}
+		} else {
+			if (!reverse) {
+				this.rigidbody.velocity = new Vector3 (0, -fallSpeed, 0);
+				fallSpeed += 2;
+				reverse = rigidbody.transform.position.y <= startPointY;
+			} else {
+				this.rigidbody.velocity = new Vector3 (0, 5, 0);
+				if(rigidbody.transform.position.y >= startPointY + 10f) {
+					reverse = false;
+					fallSpeed = 2f;
+				}
+			}
+		}
+	}
+}
