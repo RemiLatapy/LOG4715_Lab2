60f6d1d814e6c11f8457603a466b25b2fdf096fd
Sonia Ghabi <sonia.ghabi@gmail.com>
2015-12-05 00:15:16 -0500

    Moving platforms on the lake

---
+++ Assets/Sample Assets/Moving Obstacles/Scripts/HoldCar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldCar : MonoBehaviour {
+	public MovingPlatforms mp;
+
+	IEnumerator OnTriggerEnter(Collider col) {
+		Debug.Log (col.transform);
+		yield return new WaitForSeconds(2.3f);
+		col.transform.parent.parent.parent.parent = gameObject.transform;
+		mp.ChangeTarget ();
+	}
+
+	IEnumerator OnTriggerExit(Collider col) {
+		col.transform.parent.parent.parent.parent = null;
+		yield return new WaitForSeconds(2);
+		mp.ChangeTarget ();
+	}
+}
---
+++ Assets/Sample Assets/Moving Obstacles/Scripts/MovingPlatforms.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatforms : MonoBehaviour {
+	public Transform movingPlatform;
+	public Transform position1;
+	public Transform position2;
+	private Vector3 newPosition;
+	private string currentState;
+	public float smooth;
+
+	// Use this for initialization
+	void Start () {
+		currentState = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (currentState != "" && (movingPlatform.position != position1.position || movingPlatform.position != position2.position)) {
+			movingPlatform.position = Vector3.Lerp (movingPlatform.position, newPosition, smooth * Time.deltaTime);
+		}
+	}
+
+	public void ChangeTarget () {
+		if (currentState == "Moving To Position 1") {
+			currentState = "Moving To Position 2";
+			newPosition = position2.position;
+		} else if (currentState == "Moving To Position 2") {
+			currentState = "Moving To Position 1";
+			newPosition = position1.position;
+		} else if (currentState == "") {
+			currentState = "Moving To Position 2";
+			newPosition = position2.position;
+		}
+	}
+}
