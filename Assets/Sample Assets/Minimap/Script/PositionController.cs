7b5aec8bf1dba21314e697d8134018296f88502f
Sonia Ghabi <sonia.ghabi@gmail.com>
2015-12-06 00:55:31 -0500

    Minimap

---
+++ Assets/Sample Assets/Minimap/Script/PositionController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionController : MonoBehaviour {
+	public Transform attachedCar;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.position = attachedCar.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = attachedCar.position;
+	}
+}
