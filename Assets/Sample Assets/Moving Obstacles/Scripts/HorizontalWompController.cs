577ddd30af5dce95130f4fb0e0d852e8865b9d04
Sonia Ghabi <sonia.ghabi@gmail.com>
2015-12-06 16:48:38 -0500

    Womps placed

---
+++ Assets/Sample Assets/Moving Obstacles/Scripts/HorizontalWompController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalWompController : MonoBehaviour {
+	private float startPointX;
+	private float startPointZ;
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private bool zAxis;
+	[SerializeField]
+	private bool oppositeDirection;
+	bool reverse;
+	
+	
+	// Use this for initialization
+	void Start () {
+		startPointX = rigidbody.transform.position.x;
+		startPointZ = rigidbody.transform.position.z;
+		reverse = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!reverse) {
+			if (!zAxis) {
+				if(oppositeDirection){
+					this.rigidbody.velocity = new Vector3 (-speed, 0, 0);
+					reverse = rigidbody.transform.position.x <= startPointX - 25f;
+				}
+				else {
+					this.rigidbody.velocity = new Vector3 (speed, 0, 0);
+					reverse = rigidbody.transform.position.x >= startPointX + 25f;
+				}
+			} else if (zAxis) {
+				if(oppositeDirection){
+					this.rigidbody.velocity = new Vector3 (0, 0, -speed);
+					reverse = rigidbody.transform.position.z <= startPointZ - 25f;
+				}
+				else {
+					this.rigidbody.velocity = new Vector3 (0, 0, speed);
+					reverse = rigidbody.transform.position.z >= startPointZ + 25f;
+				}
+			}
+		} else {
+			if (!zAxis) {
+				if(oppositeDirection){
+					this.rigidbody.velocity = new Vector3 (speed, 0, 0);
+					reverse = !(rigidbody.transform.position.x >= startPointX);
+				}
+				else {
+					this.rigidbody.velocity = new Vector3 (-speed, 0, 0);
+					reverse = !(rigidbody.transform.position.x <= startPointX);
+				}
+
+			} else if (zAxis) {
+				if(oppositeDirection){
+					this.rigidbody.velocity = new Vector3 (0, 0, speed);
+					reverse = !(rigidbody.transform.position.z >= startPointZ);
+				}
+				else {
+					this.rigidbody.velocity = new Vector3 (0, 0, -speed);
+					reverse = !(rigidbody.transform.position.z <= startPointZ);;
+				}
+
+			}
+		}
+	}
+}
---
+++ Assets/Sample Assets/Moving Obstacles/Scripts/VerticalWompController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalWompController : MonoBehaviour {
+	private float startPointY;
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private float fallSpeed;
+	bool reverse;
+	private float initializedFallSpeed;
+
+	// Use this for initialization
+	void Start () {
+		startPointY = rigidbody.transform.position.y;
+		initializedFallSpeed = fallSpeed;
+		reverse = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!reverse) {
+			this.rigidbody.velocity = new Vector3 (0, -fallSpeed, 0);
+			fallSpeed += 2;
+			reverse = rigidbody.transform.position.y <= startPointY;
+		} else {
+			this.rigidbody.velocity = new Vector3 (0, speed, 0);
+			if(rigidbody.transform.position.y >= startPointY + 10f) {
+				reverse = false;
+				fallSpeed = initializedFallSpeed;
+			}
+		}
+	}
+}
